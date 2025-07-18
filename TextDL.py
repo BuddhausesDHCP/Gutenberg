bfc43216f568a66fd59141143f2fcea72fbf0a29
Ryan <95461278+BuddhausesDHCP@users.noreply.github.com>
2025-07-18 10:59:57 -0400

    Update TextDL.py

--- TextDL.py
+++ TextDL.py
@@ -1,14 +1,21 @@
-# Requires the Requests library to be installed before execution
+"""
+Download a UTF-8 encoded text file from Project Gutenberg and save it to a specified path.
+
+Requires:
+- Python 3.6+
+- requests library (`pip install requests`)
+"""
 
 
 import requests
 from pathlib import Path
 
 # URL of Text File (UTF-8 version)
-url = "https://www.gutenberg.org/cache/epub/****/****.txt" # Enter the URL of the .txt file
+url = "https://www.gutenberg.org/cache/epub/2701/pg2701.txt" # Example: Moby Dick
 
 # Path to your Directory (including desired file name)
-path = Path('/****/****/****/directory/****.txt') # Enter the absolute path of your desired directory
+# Replace with actual value
+    path = Path("/Users/yourname/Desktop/Moby_Dick.txt") #Example Moby_Dick.txt
 
 # Download and save
 response = requests.get(url)
